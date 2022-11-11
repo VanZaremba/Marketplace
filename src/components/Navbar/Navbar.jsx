1b4ece7305360ba822b727dd507d10961fbd2973
Vanessa Stabile Zaremba <vanessa.zaremba@coimexcon.com.br>
2022-11-11 15:27:35 -0300

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,203 @@
+import React, { Component } from "react"
+import { 
+  NavbarContainer, 
+  NavbarCategory,
+  NavbarCategoriesMenu,
+  NavbarLogo,
+  NavbarCartMenu,
+  CurrencySelect,
+  Currency,
+  ArrowIcon,
+  CurrencyContainer,
+  TotalItems,
+  Icon,
+  OpenLinksButton,
+  NavBarLinkExtended,
+  NavBarExtendedContainer,
+  NavbarMenu,
+  NavBarInnerContainer
+} from './NavbarStyles'
+import {gql } from '@apollo/client';
+import {Query} from "@apollo/client/react/components"
+import { connect } from "react-redux";
+import { setCategory } from "../../store/category/categoryActions";
+import  logo  from '../../assets/logo.svg';
+import arrowUp from '../../assets/arrow-up.svg';
+import arrowDown from '../../assets/arrow-down.svg';
+import { BsCart2 } from'react-icons/bs';
+import { setCurrency } from "../../store/currency/currencyActions";
+import { withRouter } from "react-router-dom";
+import MiniCart from "../Cart/MiniCart/MiniCart";
+
+const GET_CATEGORIES = gql`
+  {
+      categories{
+        name
+    }
+  }
+`;
+
+const GET_CURRENCIES = gql`
+  {
+      currencies{
+        label
+        symbol
+    }
+  }
+`;
+
+class Navbar extends Component {
+  constructor(props){
+    super()
+    this.box = React.createRef()
+    this.state = { 
+      category: 'all',
+      symbol: '$',
+      arrowDown: true,
+      isOpen: false,
+      extendedNavBar: false
+    }
+    this.handleCurrencyChange = this.handleCurrencyChange.bind(this)
+    this.handleCategoryChange = this.handleCategoryChange.bind(this)
+    this.handleArrowChange = this.handleArrowChange.bind(this)
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+  }
+
+  handleCategoryChange(name) {
+    this.props.setCategory(name)
+    this.setState({category: name})
+  }
+
+  handleCurrencyChange(label, symbol){
+    this.props.setCurrency(label, symbol)
+  }
+
+  handleArrowChange(){
+    this.setState({arrowDown: !this.state.arrowDown})
+  }
+
+  handleOpenModal(){
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
+  componentDidMount() {
+    let element = document.getElementById("root")
+    element.addEventListener('click', this.handleOutsideClick);
+  }
+
+  handleOutsideClick = (event) => {
+    if (this.box && !this.box.current.contains(event.target) && !this.state.arrowDown) {
+      this.handleArrowChange()
+    }
+  }
+
+  render() {
+
+    return <NavbarContainer extendNavBar={this.state.extendNavBar}>
+    <OpenLinksButton onClick={() => this.setState({extendedNavBar: !this.state.extendedNavBar})}>
+      {this.state.extendedNavBar ? <>&#10005;</> : <>&#8801;</>}
+    </OpenLinksButton>
+    <NavbarCategoriesMenu>
+      <Query query={GET_CATEGORIES}>
+        {({ loading, error, data }) => {
+            if (error) return null;
+            if (loading) return null;
+            const { categories } = data
+            return categories.map((item, index) => 
+            <NavbarCategory 
+              to="/"
+              style={this.props.categoryName === item.name ? { borderBottom: '2px solid #5ece7b' } : {}}
+              key={index} 
+              onClick={() => this.handleCategoryChange(item.name)}
+            >
+              {item.name}
+            </NavbarCategory>
+          )
+        }}
+      </Query>
+    </NavbarCategoriesMenu>
+
+    <NavbarLogo>
+      <img alt="logo" src={logo}/>
+    </NavbarLogo>
+
+    <NavbarCartMenu>
+        <Query query={GET_CURRENCIES}>
+          {({ loading, error, data }) => {
+            if (error) return null;
+            if (loading) return null;
+            const { currencies } = data
+            return <>
+            <b>
+              {currencies.map((item, index) => 
+                item.label === this.props.currencyLabel ? <span key={index}>{item.symbol}</span> : ''
+              )}
+            </b>
+            <ArrowIcon ref={this.box} onClick={() => this.handleArrowChange()}>
+              <img alt="logo" src={this.state.arrowDown ? arrowDown : arrowUp}/>
+            </ArrowIcon>
+            <CurrencyContainer show={this.state.arrowDown}>
+              <CurrencySelect>
+                {currencies.map((item, index) => 
+                  <Currency key={index} onClick={() => { this.handleCurrencyChange(item.label, item.symbol); this.handleArrowChange()}}>
+                    {item.symbol}{' '}{item.label}
+                  </Currency>
+                )}
+              </CurrencySelect>
+            </CurrencyContainer>
+          </>
+          }}
+        </Query>
+        <Icon>
+          <BsCart2 onClick={this.handleCartClick, this.handleOpenModal}/>
+          <MiniCart isOpen={this.state.isOpen} handleOpenModal={this.handleOpenModal}/>
+      
+          {this.props.totalItems !== 0 &&
+            <TotalItems>
+              {this.props.totalItems}
+            </TotalItems>
+          }
+          </Icon>
+      </NavbarCartMenu> 
+  
+    {this.state.extendedNavBar && (
+        <NavBarExtendedContainer>
+          <Query query={GET_CATEGORIES}>
+            {({ loading, error, data }) => {
+                if (error) return null;
+                if (loading) return null;
+                const { categories } = data
+                return categories.map((item, index) => 
+                <NavBarLinkExtended
+                  to="/"
+                  style={this.state.category === item.name ? { borderBottom: '2px solid #5ece7b' } : {}}
+                  key={index} 
+                  onClick={() => {this.handleCategoryChange(item.name); this.setState({extendedNavBar: !this.state.extendedNavBar})} }
+                >
+                  {item.name}
+                </NavBarLinkExtended>
+              )
+            }}
+          </Query>
+        </NavBarExtendedContainer>
+      )} 
+    </NavbarContainer>
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    categoryName: state.categoryReducer.categoryName,
+    currencyLabel: state.currencyReducer.currencyLabel,
+    totalItems: state.cartReducer.totalItems
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setCategory: (name) => dispatch(setCategory(name)),
+    setCurrency: (label, symbol) => dispatch(setCurrency(label, symbol))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar));
---
+++ src/components/Navbar/NavbarStyles.js
@@ -0,0 +1,157 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavbarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 0 100px;
+  justify-content: space-between;
+  height: ${(props) => (props.extendNavBar ? "100vh" : "80px")};
+  position: fixed;
+  background-color: #fff;
+  z-index: 10;
+  @media (max-width: 700px) {
+    padding: 0;
+  }
+`;
+
+export const NavbarCategoriesMenu = styled.div`
+  display: flex;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const NavbarCategory = styled(Link)`
+  font-family: "Raleway";
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 120%;
+  color: #1d1f22;
+  cursor: pointer;
+  width: 100px;
+  height: 80px;
+  display: flex;
+  margin-right: 2px;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  text-decoration: none;
+`;
+
+export const NavbarLogo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: -100px;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const NavbarCartMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 700px) {
+    margin-right: 40px;
+  }
+`;
+
+export const CurrencySelect = styled.ul`
+  top: 70px;
+  list-style: none;
+  width: 100px;
+`;
+
+export const Currency = styled.li`
+  font-size: 18px;
+  padding-left: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #eee;
+  }
+`;
+
+export const CurrencyContainer = styled.div`
+  display: ${(props) => (props.show ? "none" : "")};
+  position: absolute;
+  top: 65px;
+  box-shadow: 0px 2px 35px rgba(168, 172, 176, 0.19);
+  z-index: 4;
+`;
+
+export const ArrowIcon = styled.div`
+  font-size: 12x;
+  width: 12px;
+  margin: 0 10px;
+  cursor: pointer;
+`;
+
+export const Icon = styled.div`
+  margin-left: 22px;
+  position: relative;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const TotalItems = styled.div`
+  background-color: black;
+  position: absolute;
+  width: 22px;
+  height: 22px;
+  color: white;
+  font-size: 12px;
+  font-family: "Roboto";
+  font-weight: 700;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: -90%;
+  right: -100%;
+`;
+
+export const OpenLinksButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0px;
+  color: black;
+  font-size: 3rem;
+  align-items: center;
+  width: 20%;
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+export const NavBarExtendedContainer = styled.div`
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  z-index: 3;
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+export const NavBarLinkExtended = styled(Link)`
+  font-size: x-large;
+  text-decoration: none;
+  margin: 1rem;
+  text-transform: uppercase;
+  color: #1d1f22;
+`;
